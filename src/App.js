d2e385eb93b467a61deb7fe7c8aeb5a5025f8ad3
Gaston Irby III <gaston@gastons-imac.lan>
2021-11-05 10:32:49 -0700

    passed all the tests, like a boss

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
   useEffect (() => {
     axios.get(`https://swapi.dev/api/people`)
       .then(res => {
-        console.log(res);
+        setSWCharacters(res.data);
       })
       .catch(err => {
         console.error(err);
